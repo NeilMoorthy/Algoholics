a6a4d5ad2e7ff27c7fdb657ef422040da7580d9b
NeilMoorthy <neilboji@gmail.com>
2024-01-31 15:36:48 +0530

    vagaaarthh this is new code

--- neil.py
+++ neil.py
@@ -1,1 +1,14 @@
-print("hello")
+import matplotlib.pyplot as p
+import numpy as np
+amt=15
+l=np.random.randint(0,100,amt)
+x=np.arange(0,amt,1)
+n=len(l)
+for i in range(n):
+    for j in range(0,n-i-1):
+        p.bar(x,l)
+        p.pause(0.01)
+        p.clf()
+        if l[j]>l[j+1]:
+            l[j],l[j+1]=l[j+1],l[j]
+p.show()
